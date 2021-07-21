718f84b6d59e7156b0de6ac1c283e79b29cf485e
chutcheson1936 <c.hutcheson1936@users.noreply.github.com>
2021-07-21 19:40:54 -0400

    Add project files.

---
+++ COP3530_Project3/main.cpp
@@ -0,0 +1,125 @@
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include "TextureManager.h"
+#include "Display.h"
+#include <iostream>
+#include "VisualElements.h"
+
+int main()
+{
+    //INITIALIZE IMPORTANT OBJECTS NECESSARY TO PRINT THINGS TO SCREEN
+    sf::RenderWindow window(sf::VideoMode(1900, 1000), "SFML works!");
+    std::map<string, Button> buttons;
+    createButtons(buttons);
+
+    std::vector<sf::Texture*> vect;
+    sf::Texture search = TextureManager::GetTexture("search");
+    sf::Texture results = TextureManager::GetTexture("results");
+
+    sf::Texture fair = TextureManager::GetTexture("fair");
+    sf::Texture cloudy = TextureManager::GetTexture("cloudy");
+    sf::Texture rain = TextureManager::GetTexture("rain");
+    sf::Texture snow = TextureManager::GetTexture("snow");
+    sf::Texture fair_np = TextureManager::GetTexture("fair_notpressed");
+    sf::Texture cloudy_np = TextureManager::GetTexture("cloudy_notpressed");
+    sf::Texture snow_np = TextureManager::GetTexture("snow_notpressed");
+    sf::Texture rain_np = TextureManager::GetTexture("rain_notpressed");
+
+    std::map<string, sf::ConvexShape> states;
+    loadStates(states);
+
+
+    vect.push_back(&fair);
+    vect.push_back(&cloudy);
+    vect.push_back(&rain);
+    vect.push_back(&snow);
+    vect.push_back(&fair_np);
+    vect.push_back(&cloudy_np);
+    vect.push_back(&rain_np);
+    vect.push_back(&snow_np);
+
+
+    sf::String cityText = "";
+    sf::String stateText = "";
+
+    sf::Text city;
+    sf::Text state;
+
+    city.setString("");
+    state.setString("");
+
+    sf::Font font;
+    font.loadFromFile("Font/Montserrat-Medium.ttf");
+
+    city.setFont(font);
+    city.setCharacterSize(25);
+
+    state.setFont(font);
+    state.setCharacterSize(25);
+
+
+    //WHILE WINDOW IS OPEN
+    while (window.isOpen())
+    {
+
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            //IF WINDOW IS CLOSED
+            if (event.type == sf::Event::Closed) {
+                window.close();
+                TextureManager::Clear();
+            }
+
+            //If left click is detected, get coordinates and see if a button was pressed
+            else if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                auto coordinates = sf::Mouse::getPosition(window);
+                findButtonPressed(coordinates, buttons);
+
+            }
+
+            //if text is entered, check if a text box was pressed; If it was, "echo" the character back to screen
+            else if (event.type == sf::Event::TextEntered) {
+                if (buttons["City"].isPressed) {
+
+                    if (event.text.unicode == '\b') { //backspace -> remove a character from end
+                        if (cityText.getSize() != 0) {
+                            cityText.erase(cityText.getSize() - 1);
+                        }
+                    }
+                    else {
+                        cityText = cityText + event.text.unicode;
+                    }
+                }
+                else if (buttons["State"].isPressed) {
+                    if (event.text.unicode == '\b') {
+                        if (stateText.getSize() != 0) {
+                            stateText.erase(stateText.getSize() - 1);
+                        }
+                    }
+                    else {
+                        stateText = stateText + event.text.unicode;
+                    }
+                }
+
+                //update the string
+                city.setPosition(sf::Vector2f(40, 530));
+                city.setString(cityText);
+
+                state.setPosition(sf::Vector2f(40, 605));
+                state.setString(stateText);
+
+                window.draw(city);
+                window.draw(state);
+
+            }
+
+        }
+
+        //display new board
+        window.clear();
+        displayNewBoard(window, buttons, &search, &results, vect, states, city, state);
+    }
+
+    return 0;
+}
